58394fd906305d0867b8a480a47ab3cb5680378f
bartosz.nakonieczny <bartek721127@interia.pl>
2021-11-04 20:48:07 +0100

    zapis cad->cad2010 w tym samym folderze

--- cad_to_eplan.py
+++ cad_to_eplan.py
@@ -15,9 +15,17 @@ time.sleep(3)
 # gui.click()
 # gui.press('escape')
 
-page_list = [21, 23, 25, 26, 27, 31, 37, 62, 63, 64, 65, 72, 73, 74, 76, 77, 78, 80, 83, 84, 85]
+page_list = ['005', '006', '007', '008', '009', '010', '011', '012', '013', '021', '022',
+             '023', '024', '025', '026', '027', '028', '029', '030', '031', '032', '033', '034', '035', '036', '037',
+             '038', '039', '041', '042', '043', '044', '045', '046', '047', '048', '049', '050', '051', '052A', '052B',
+             '052C', '052D', '052E', '052F', '052G', '052H', '053A', '053B', '053C', '053D', '061', '063', '065', '069',
+             '070', '072', '072A', '073', '074', '075', '080', '081', '083', '084', '085', '088', '089', '090', '091',
+             '104', '105', '107', '108', '109', '110', '111', '112', '113', '114', '115', '116', '117', '118', '119',
+             '121', '122', '123', '130', '131', '132', '133', '134', '135', '136', '138', '139', '140', '141', '142',
+             '143', '191', '192', '193', '195', '196', '197', '198', '201', '202', '203', '204', '205', '206', '211',
+             '221', '222', '231', '232']
 
-sheet_list = [32, 34, 36, 37, 38, 44, 61, 127, 128, 131, 132, 183, 184, 185, 196, 197, 198, 200, 213, 221, 222]
+sheet_list = page_list
 
 for page_nr, sheet_nr in zip(page_list, sheet_list):
     time.sleep(0.8)
@@ -30,7 +38,7 @@ for page_nr, sheet_nr in zip(page_list, sheet_list):
 
     # czekam aż pojawi się okno, a jeśli długo się nie pojawi to raz jeszcze skrót klawiszowy
     temp_count = 0
-    while gui.pixel(624, 645)[0] != 240:
+    while gui.pixel(1019, 556)[0] != 240:
         time.sleep(0.1)
         temp_count = temp_count + 1
 
@@ -42,7 +50,7 @@ for page_nr, sheet_nr in zip(page_list, sheet_list):
             gui.keyUp('shift')
 
         # wpisuje nazwe pliku
-    gui.typewrite('bb3_' + str(page_nr) + '.dwg', interval=0.05)
+    gui.typewrite(page_nr + '_BB1.dwg', interval=0.05)
     gui.press('enter')
 
     # czekam, aż pojawi się okno ze źródłem i schematem
@@ -79,7 +87,7 @@ for page_nr, sheet_nr in zip(page_list, sheet_list):
     #     time.sleep(0.1)
 
     # podaje numer strony
-    gui.typewrite(str(sheet_nr), interval=0.05)
+    gui.typewrite(sheet_nr, interval=0.05)
 
     # zatwierdzam numer
     gui.press('enter')
